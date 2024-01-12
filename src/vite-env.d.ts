8fafe55894bb4b0278831ce23b9bd889e8f093fa
Sean Stocker <sean.stocker14@gmail.com>
2024-01-12 20:55:31 +0000

    Small Changes feat: moved typescript.svg from src to public feat: added description and author to package.json feat: added global type for SVGs feat: configured tsconfig and vite.config to enable a 'src' alias

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,2 @@
+/// <reference types="./types/svg.d.ts" />
 /// <reference types="vite/client" />
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
+  },
+});
